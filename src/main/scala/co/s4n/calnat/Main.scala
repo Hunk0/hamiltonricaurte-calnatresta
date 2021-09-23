31caf1938959cfb8e4d86cc877f334c418ff1642
Hunk0 <hricaurtec@correo.udistrital.edu.co>
2021-09-23 15:28:49 -0500

    noimplementation v2

--- src/main/scala/co/s4n/calnat/Main.scala
+++ src/main/scala/co/s4n/calnat/Main.scala
@@ -8,26 +8,34 @@ object Main extends App {
         s.toInt
     }
     def esCero(nat: Nat) = nat match {
-        case Cero => true
+        case Cero() => true
         case Suc(nat) => false
     }
     def esMayorIgual(nat1: Nat, nat2: Nat): Boolean = nat1 match {
-        case Cero => nat2 match {
-            case Cero => true
+        case Cero() => nat2 match {
+            case Cero() => true
             case _      => false
          }
         case Suc(pnat) => nat2 match {
-            case Cero => true
+            case Cero() => true
             case Suc(snat) => esMayorIgual(pnat, snat)
         }
     }
-    def conIntANat(i : Int): Nat = ???
+    def conIntANat(i : Int): Nat = {
+        def iConv(sts: Nat, acum: Int): Nat = acum match {
+            case 0 => sts
+            case acum => iConv(Suc(sts), acum - 1)
+        }
+
+        iConv(Cero(), i)
+    }
     def imprimirNat(nat: Nat): String = ???
     def restaNat(nat1: Nat, nat2: Nat): Nat = ???
 
     val entero1 = leetInt("Leer primer entero ")
     val entero2 = leetInt("Leer segundo entero ")
-    //val nat1 = copyIntANat(entero1)
+    val nat1 = conIntANat(entero1)
+    val nat2 = conIntANat(entero2)
 
     //println(restaNat(entero1, entero2))
 }
--- src/main/scala/co/s4n/calnat/Nat.scala
+++ src/main/scala/co/s4n/calnat/Nat.scala
@@ -1,15 +1,5 @@
 package co.s4n.calnat
 
-sealed trait Nat {
-    def ++ : Nat = Suc(this)
-    def +(x: Nat): Nat
-    def *(x: Nat): Nat
-}
-case object Cero extends Nat {
-    def +(x: Nat): Nat = x
-    def *(x: Nat): Nat = Cero
-}
-case class Suc(nat: Nat) extends Nat {
-    def +(x: Nat): Nat = Suc(nat.+(x))
-    def *(x: Nat): Nat = nat.*(x).+(x)
-}
+trait Nat
+case class Cero() extends Nat
+case class Suc(nat: Nat) extends Nat
